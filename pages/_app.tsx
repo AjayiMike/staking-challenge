7175ec1e1e37daaabd926f6b8cacacde453a04b1
AjayiMike <ajayimikeofficial@gmail.com>
2022-03-02 19:46:40 +0100

    configure degen design system and created a simple app header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'degen'
+import 'degen/styles'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
